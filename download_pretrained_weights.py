fc3c59a4ba09fb1d3ca17faafb5658a7522ab0ae
lixirui142 <lixirui142@sjtu.edu.cn>
2025-05-11 18:48:18 +0800

    fix typo

--- download_pretrained_weights.py
+++ download_pretrained_weights.py
@@ -2,5 +2,5 @@ import os
 from huggingface_hub import snapshot_download
 
 weight_dir = "./weights"
-os.makedirs(weight_dir, exists_ok=True)
+os.makedirs(weight_dir, exist_ok=True)
 snapshot_download(repo_id="lixirui142/unicon", local_dir=weight_dir)
